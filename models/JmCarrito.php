6f7e4c916ef924b67272f53f7de253bc4d9432b5
Marshelott25 <72777535+Marshelott25@users.noreply.github.com>
2022-03-11 23:50:22 -0600

    Prueba7

--- models/JmCarrito.php
+++ models/JmCarrito.php
@@ -49,7 +49,7 @@ class JmCarrito extends \yii\db\ActiveRecord
         return [
             'car_id'            => 'Id',
             'car_cantidad'             => 'Cantidad',
-            'car_descripcion'             => 'Descripcion',
+            'car_descripcion'                 => 'Descripcion',
             'car_fkmodelo'      => 'Modelo',
             'car_fkventa'       => 'Venta',
         ];
